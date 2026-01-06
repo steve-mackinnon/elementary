369e400d0f67d119037d7ffbe18b1e8747b14cc2
Steve MacKinnon <mackinnon.st@gmail.com>
2026-01-06 00:06:31 -0500

    Add some test graphs for sample nodes and a test runner script that exports each graph to file on disk

---
+++ cli/examples/test_graphs/SampleTest.js
@@ -0,0 +1,10 @@
+import { el, Renderer } from "@elemaudio/core";
+
+const core = new Renderer((batch) => {
+	__postNativeMessage__(JSON.stringify(batch));
+});
+
+const sample = el.sample({path: "testtone", channels: 2}, el.train(1), 1);
+const stats = core.render(sample, sample);
+
+console.log(stats);
