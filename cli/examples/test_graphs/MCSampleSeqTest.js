f2dffb6a9b9f331bc338944a565f98a57ef69ccc
Steve MacKinnon <mackinnon.st@gmail.com>
2026-01-06 21:21:49 -0500

    Add test graphs for all sample nodes

---
+++ cli/examples/test_graphs/MCSampleSeqTest.js
@@ -0,0 +1,22 @@
+import { el, Renderer } from "@elemaudio/core";
+
+const core = new Renderer((batch) => {
+  __postNativeMessage__(JSON.stringify(batch));
+});
+
+const [left, right] = el.mc.sampleseq(
+  {
+    path: "testtone",
+    channels: 2,
+		duration: 1,
+    seq: [
+      { time: 0, value: 1 },
+      { time: 0.5, value: 0 },
+      { time: 0.6, value: 1 },
+    ],
+  },
+	el.phasor(0.7)
+);
+const stats = core.render(left, right);
+
+console.log(stats);
---
+++ cli/examples/test_graphs/SampleSeq2StretchTest.js
@@ -0,0 +1,23 @@
+import { el, Renderer } from "@elemaudio/core";
+
+const core = new Renderer((batch) => {
+  __postNativeMessage__(JSON.stringify(batch));
+});
+
+const out = el.sampleseq2(
+  {
+    path: "testtone",
+    duration: 1,
+    stretch: 2,
+    shift: -7,
+    seq: [
+      { time: 0, value: 1 },
+      { time: 0.5, value: 0 },
+      { time: 0.6, value: 1 },
+    ],
+  },
+  el.phasor(0.7)
+);
+const stats = core.render(out, out);
+
+console.log(stats);
